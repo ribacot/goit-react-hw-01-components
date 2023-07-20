5db19cbeeb0655af580d4eb512c30a0ee0f9e41f
ribacot <remontdiskov1979@gmail.com>
2023-07-20 18:20:11 +0300

    apd_css3

--- src/components/friendListItem/FriendListItem.jsx
+++ src/components/friendListItem/FriendListItem.jsx
@@ -1,9 +1,15 @@
+import './style.css';
 export default function FriendListItem({ data }) {
   return data.length
     ? data.map(({ avatar, name, isOnline, id }) => {
+        let statusColor = null;
+        isOnline ? (statusColor = 'green') : (statusColor = 'red');
         return (
           <li key={id} className="item">
-            <span className="status">{isOnline}</span>
+            <span
+              className="status"
+              style={{ backgroundColor: statusColor }}
+            ></span>
             <img className="avatar" src={avatar} alt="User avatar" width="48" />
             <p className="name">{name}</p>
           </li>
