7e2edbcafc5844409d0aab3be9c821c66c8f0ad3
ribacot <remontdiskov1979@gmail.com>
2023-07-23 12:56:44 +0300

    fix_tr

--- src/components/transaction-table-items/TransactoinTableItems.jsx
+++ src/components/transaction-table-items/TransactoinTableItems.jsx
@@ -4,7 +4,7 @@ import css from './TransactoinTableItems.module.css';
 export default function TransactoinTableItems({ items }) {
   return items.map(({ id, type, amount, currency }, index) => {
     return (
-      <tr key={id} className={index % 2 && css.grey}>
+      <tr key={id} >
         <td>{type}</td>
         <td>{amount}</td>
         <td>{currency}</td>
